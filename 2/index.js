4ba8f0018ddfd5733fc99f53f5220f63143f3b2d
Pierre Beaujeu <pierre.beaujeu@gmail.com>
2021-12-01 10:39:25 +0100

    puzzle #2

---
+++ 2/index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+console.log("AOC #2");
+
+const data = fs.readFileSync(path.resolve(__dirname, "./input"), "utf-8");
+
+const increases = data
+  .split("\n")
+  .map((num) => parseInt(num, 10))
+  .reduce(
+    (tmw, current, index, array) =>
+      index < 2 ? tmw : [...tmw, array[index - 2] + array[index - 1] + current],
+    []
+  )
+  .reduce(
+    (sum, current, index, array) =>
+      !index ? 0 : array[index - 1] < current ? sum + 1 : sum,
+    0
+  );
+
+console.log("Result:", increases);
