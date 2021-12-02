32342b5409d0b78552b799f5afbefb6c397cdafe
Pierre Beaujeu <pierre.beaujeu@gmail.com>
2021-12-02 10:35:27 +0100

    rework structure per day

--- run-all.sh
+++ run-all.sh
@@ -2,6 +2,7 @@
 
 for d in */ ; do
   [ -z "$sep" ] && sep="true" || echo "\n---------------------\n";
-  echo "Advent of code #${d//[^0-9.]/}"
-  node "$d"
+  echo "Advent of code day #${d//[^0-9.]/}"
+  [ -f "$d/1.js" ] && echo "\nChallenge 1" && node "$d/1"
+  [ -f "$d/2.js" ] && echo "\nChallenge 2" && node "$d/2"
 done
