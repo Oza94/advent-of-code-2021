b794575a60f19020f8a5b7b6895f7171a628b95d
Pierre Beaujeu <pierre.beaujeu@gmail.com>
2021-12-19 14:48:27 +0100

    puzzle #36

---
+++ day-18/2.js
@@ -0,0 +1,111 @@
+const ctrl = "[],";
+
+function parse(expr) {
+  return expr.split("").map((c) => (ctrl.includes(c) ? c : parseInt(c)));
+}
+
+const data = require("fs")
+  .readFileSync(require("path").resolve(__dirname, "./input"), "utf-8")
+  .split("\n")
+  .map(parse);
+
+function addExplode(snailnum, i, side, n) {
+  while (i > -1 && i < snailnum.length) {
+    if (Number.isInteger(snailnum[i])) {
+      snailnum[i] += n;
+      return;
+    }
+    i += side;
+  }
+}
+
+function reduceOnce(initSn) {
+  const snailnum = [...initSn];
+  let depth = 0;
+  let side = -1;
+  let split = null;
+  for (let i = 0; i < snailnum.length; i++) {
+    const tok = snailnum[i];
+    switch (tok) {
+      case "[":
+        depth += 1;
+        side = -1;
+        break;
+      case "]":
+        if (depth > 4) {
+          snailnum.splice(i - 4, 5, 0);
+          return [snailnum, false];
+        }
+        depth -= 1;
+        break;
+      case ",":
+        side = 1;
+        break;
+      default:
+        if (depth > 4) {
+          addExplode(snailnum, i + side, side, tok);
+        } else if (tok > 9 && !split) {
+          split = [i, tok];
+        }
+        break;
+    }
+  }
+
+  if (split) {
+    snailnum.splice(
+      split[0],
+      1,
+      "[",
+      Math.floor(split[1] / 2),
+      ",",
+      Math.ceil(split[1] / 2),
+      "]"
+    );
+    return [snailnum, false];
+  }
+  return [snailnum, true];
+}
+
+function reduce(snailnum) {
+  let noop = false;
+  do {
+    [snailnum, noop] = reduceOnce(snailnum);
+  } while (!noop);
+  return snailnum;
+}
+
+function add(s1, s2) {
+  return reduce(["[", ...s1, ",", ...s2, "]"]);
+}
+
+function magnitude(initSn) {
+  const snailnum = [...initSn];
+  do {
+    for (let i = 0; i < snailnum.length; i++) {
+      const tok = snailnum[i];
+      if (
+        tok === "," &&
+        Number.isInteger(snailnum[i - 1]) &&
+        Number.isInteger(snailnum[i + 1])
+      ) {
+        const m = snailnum[i - 1] * 3 + snailnum[i + 1] * 2;
+        snailnum.splice(i - 2, 5, m);
+        break;
+      }
+    }
+  } while (snailnum.length > 1);
+  return snailnum[0];
+}
+
+function largestMagnitude(snailnums) {
+  let largest = 0;
+  for (let i = 0; i < snailnums.length; i++) {
+    for (let j = 0; j < snailnums.length; j++) {
+      largest = Math.max(largest, magnitude(add(snailnums[i], snailnums[j])));
+    }
+  }
+
+  return largest;
+}
+
+console.log("Result:", largestMagnitude(data));
